b6c66c0398ba50d6fc9f628405e57f31740731bb
sshaunn <sp100809@gmail.com>
2022-05-31 14:29:22 +1000

    moving to windows

--- route/src/main/java/com/dev/route/RouteApplication.java
+++ route/src/main/java/com/dev/route/RouteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
+//@EnableHystrix
 public class RouteApplication {
 
 	public static void main(String[] args) {

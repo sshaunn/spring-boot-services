99f7035811c1b31da90a4491f726b6a9a31d0139
sshaunn <sp100809@gmail.com>
2022-05-31 19:31:12 +1000

    test

--- employee/src/main/java/com/dev/employee/repository/EmployeeRepository.java
+++ employee/src/main/java/com/dev/employee/repository/EmployeeRepository.java
@@ -9,3 +9,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Employee findEmployeeByEmployeeId(Long employeeId);
 }
+    git init
+    git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/sshaunn/springApp.git
+        git push -u origin main
